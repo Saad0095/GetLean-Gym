49213add6a4906d7bca7f7adc4c2b9105c0830ff
Saad0095 <saadkhalid0095@gmail.com>
2024-03-01 23:37:02 +0500

    Added Facilities Section, About Us and Contact Us

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var form = document.querySelector('form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var name = document.getElementById('exampleInputName').value;
+        var email = document.getElementById('exampleInputEmail1').value;
+        var message = document.getElementById('exampleFormControlTextarea1').value;
+        if (!name || !email || !message) {
+            alert("Please fill in all details before submitting");
+            return;
+        }
+        if (/\d/.test(name)) {
+            alert("Name should not contain numbers");
+            return;
+        }
+        if (!/@/.test(email)) {
+            alert("Invalid email address. Email should contain @");
+            return;
+        }
+        alert("Member added successfully");
+    });
+});
